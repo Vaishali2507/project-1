0a91e61eea1785c7c1226d49ee919931f891c468
Vaish2507 <vaishali,prakasan25@gmail.com>
2026-09-21 21:51:46 +0530

    add new class

---
+++ src/Daywise_classnotes/operators.java
@@ -0,0 +1,14 @@
+package Daywise_classnotes;
+
+public class operators {
+    int a = 20;
+    int b = 30;
+    system.out.println(++a);
+    system.out.println(--a);
+    system.out.println(++b);
+    system.out.println(b++);
+    int y;
+    y = a++ + ++b;
+    system.out.println(y);
+
+}
---
+++ src/Daywise_classnotes/operators1.java
@@ -0,0 +1,14 @@
+package Daywise_classnotes;
+
+public class operators1 {
+	public static void main(String[] args) {
+	int a = 20;
+    int b = 30;
+    System.out.println(++a);
+    System.out.println(--b);
+    int y;
+    y = a++ + ++b;
+    System.out.println(y);
+
+}
+}
